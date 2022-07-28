42fb29349cf757b110375a8cb8880c8acaf80103
Julian03-lab <julian03yt@gmail.com>
2022-07-27 23:39:55 -0300

    Remove finished task

--- app.js
+++ app.js
@@ -4,26 +4,38 @@ const finishedTask = document.querySelector("#task-finished");
 const input = document.getElementById("task-input");
 
 addElement.addEventListener("click", () => {
-  addTask();
+  if (input.value != '') {
+    addTask();
+    
+  }
 });
 
 input.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
+  if (e.key === "Enter" && input.value != '') {
     addTask();
   }
 });
 
 taskList.addEventListener("click", (e) => {
   if (e.target.className === "fa-solid fa-check") {
+    e.target.className = 'fa-solid fa-rotate-left'
     parentTask = e.target.parentElement.parentElement;
     finishedTask.appendChild(parentTask);
+  }
+});
+
+finishedTask.addEventListener("click", (e) => {
+  if (e.target.className === "fa-solid fa-rotate-left") {
+    e.target.className = 'fa-solid fa-check'
+    parentTask = e.target.parentElement.parentElement;
+    taskList.appendChild(parentTask);
   }
 });
 
 function addTask() {
   let task = document.createElement("div");
   task.innerHTML = `<p></p><button class="btn"><i class="fa-solid fa-check"></i></button>`;
-  task.innerText = input.value
+  task.children[0].innerText = input.value
   task.classList = "task";
   taskList.appendChild(task);
   input.value = input.defaultValue;
